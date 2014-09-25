bf3f91c2b449aa4e1a399ed8f943d434264185dd
Martin Hron <hron@avast.com>
2014-09-25 19:46:27 +0200

    very late touches

--- Sources/ServiceException1/ServiceException1/ServiceException1.cpp
+++ Sources/ServiceException1/ServiceException1/ServiceException1.cpp
@@ -4,42 +4,73 @@
 #include "stdafx.h"
 #include "windows.h"
 #include <stdio.h>
-
+#define PATTERNSIZE 0x200
 int _tmain(int argc, _TCHAR* argv[])
 {
-	printf("Verifying that a pattern left on the stack is not smashed by SMC.\n");
+	
+	printf("Verifying that a pattern left on the stack is not smashed by SMC.");
 
 	DWORD  oldProt;
 	VirtualProtect(_tmain, 8192, PAGE_EXECUTE_READWRITE, &oldProt);
-
-	unsigned int pattern1, pattern2, word1, word2;
+	void* firstchangeaddr = NULL;
+	unsigned char* bufferptr = (unsigned char*)VirtualAlloc(NULL, PATTERNSIZE, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 
 	_asm {
-		push 0xdeadbeef
-		push 0xbadcafe1
-		pop pattern2
-		pop pattern1
+		
 
+	    mov edi,esp
+		sub edi, PATTERNSIZE
+		mov ecx, PATTERNSIZE
+		mov eax, 65
+		
+		rep stosb
+		
 		mov byte ptr smc, 0x90
+
 smc:
 		_emit 0xcc
-		sub esp, 8
-		pop word2
-		pop word1
+		
+
+		mov esi, esp
+		mov edi, bufferptr;
+		sub esi, PATTERNSIZE
+		mov ecx, PATTERNSIZE
+		rep movsb
 
 	}
 
-	printf("* [ESP-4] before SMC: %x\n", pattern1);
-	printf("* [ESP-8] before SMC: %x\n", pattern2);
-	printf("* [ESP-4] after SMC: %x\n", word1);
-	printf("* [ESP-8] after SMC: %x\n", word2);
+	bool passed = true;
+	int idx = 0;
+	for (int i= PATTERNSIZE-1; i>=0; i--) {
+		
+		if ((idx++ % 32)==0 )
+			printf("\n [ESP-%04x]  ", (PATTERNSIZE-1) - i);
 
-	if (pattern1 == word1 && pattern2 == word2) {
-		printf("Test passed.\n");
+		printf("%c", bufferptr[i] != 65 ? 'X' : '.');
+		
+		if (bufferptr[i] != 65) {
+			
+			if (!firstchangeaddr)
+				firstchangeaddr = &bufferptr[i];
+
+			passed = false;
+		}
+
+
+	}
+
+	printf("\n");
+
+	printf("> Address of first difference: %p\n", firstchangeaddr);
+
+	VirtualFree(bufferptr, PATTERNSIZE, MEM_RELEASE);
+
+	if (passed) {
+		printf("> Test passed.\n");
 		return 0;
 	}
 
-	printf("Test failed.\n");
+	printf("> Test failed.\n");
 	return 1;
 }
 
